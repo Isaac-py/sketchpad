c8ffd20de67a225e91fb440f09d9f41f7566ddb2
Aziret-Ali Ismailov <ismailovisaac@gmail.com>
2025-06-12 12:00:15 -0400

    Implement function: change grid background color on hover

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const container = document.querySelector("#container");
 for(let i=0;i<256;i++){
     const content = document.createElement("div");
     content.classList.add("grid");
-    content.textContent = i;
     container.appendChild(content);
+    content.addEventListener('mouseenter',() => content.style.backgroundColor = "blue");
 }
+
+
