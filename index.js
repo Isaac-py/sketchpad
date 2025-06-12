ed2b65603d505fab06089d6eb9bcda215718d196
Aziret-Ali Ismailov <ismailovisaac@gmail.com>
2025-06-12 16:32:38 -0400

    Implement function to prompt new grid size and create it

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const container = document.querySelector("#container");
-for(let i=0;i<256;i++){
-    const content = document.createElement("div");
-    content.classList.add("grid");
-    container.appendChild(content);
-    content.addEventListener('mouseenter',() => content.style.backgroundColor = "blue");
-}
 const btn = document.querySelector('#reset');
+
+function createGrid(size){
+    container.innerHTML = '';
+
+
+    const cellSize = 600/size;
+    for(let i=0;i<size*size;i++){
+        const content = document.createElement("div");
+        content.classList.add("grid");
+        content.style.width = cellSize+'px';
+        content.style.height = cellSize+'px';
+        container.appendChild(content);
+        content.addEventListener('mouseenter',() => content.style.backgroundColor = "blue");
+    }
+
+}
+createGrid(16);
 btn.addEventListener('click', () => {
-    const content = Array.from(document.querySelectorAll('.grid'));
-    content.forEach(content => {
-        content.style.backgroundColor = "lightblue"; 
-    });
+    let newSize = prompt("Enter new grid size (1–100):");
+    newSize = parseInt(newSize);
+
+    if (!isNaN(newSize) && newSize > 0 && newSize <= 100) {
+        createGrid(newSize);
+    } else {
+        alert("Please enter a number between 1 and 100.");
+    }
     }
 );
